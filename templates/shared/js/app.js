5c3a65cb11593114f7f9c50dfe66406eb8c0d563
ToNyOyO <tonyknibb@hotmail.com>
2020-05-11 11:45:33 +0100

    Add files via upload

--- templates/shared/js/app.js
+++ templates/shared/js/app.js
@@ -1,21 +1,21 @@
 $(function() {
 
     // PUBLISH
-    const isPublished = false;
+    var isPublished = false;
 
     // links
-    const pagesLink = $('.pages-link');
-    const refsLink = $('.refs-link');
-    const piLink = $('.pi-link');
+    var pagesLink = $('.pages-link');
+    var refsLink = $('.refs-link');
+    var piLink = $('.pi-link');
 
     // page elements
-    const refsOverlay = $('#refs-overlay');
-    const references = $('#references');
-    const refCloseBtn = $('.ref-close-button');
+    var refsOverlay = $('#refs-overlay');
+    var references = $('#references');
+    var refCloseBtn = $('.ref-close-button');
 
-    const popupOverlay = $('#popup-overlay');
-    const popup = $('.popup-link');
-    const popCloseBtn = $('.pop-close-button');
+    var popupOverlay = $('#popup-overlay');
+    var popup = $('.popup-link');
+    var popCloseBtn = $('.pop-close-button');
 
     /******************************************************************************
      * References link (toggle)
@@ -35,7 +35,7 @@ $(function() {
     popup.on('click', function (e) {
         e.preventDefault();
 
-        let popupId = $(this).attr('data-popup-id');
+        var popupId = $(this).attr('data-popup-id');
 
         popupOverlay.addClass('show');
         $('body').find('#'+popupId).addClass('show');
@@ -63,15 +63,15 @@ $(function() {
         $(this).parent().parent().removeClass('show');
     });
 
-    
-     /******************************************************************************
+
+    /******************************************************************************
      * Links out to other presentations
      *
      */
 
     /** INSERT LINK TO OTHER PRES HERE **/
 
-    
+
     /******************************************************************************
      * VEEVA MENU LINKS
      */
@@ -82,7 +82,7 @@ $(function() {
 (function($) {
     $.fn.animateNumbers = function(start, stop, commas, duration, delay, ease) {
         return this.each(function() {
-            let $this = $(this);
+            var $this = $(this);
             start = (start !== null) ? start : parseInt($this.text().replace(/,/g, "")) ;
             commas = (commas === undefined) ? true : commas;
             delay = (delay === undefined) ? 0 : delay;
